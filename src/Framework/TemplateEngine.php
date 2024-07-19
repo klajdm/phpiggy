403cf0b953d38fbe46362a0dde8b39c0f9a54028
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-19 16:55:16 +0200

    added about page

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -11,8 +11,23 @@ class TemplateEngine
     }
 
 
-    public function render(string $template)
+    public function render(string $template, array $data = [])
     {
-        include "{$this->basePath}/{$template}";
+        extract($data, EXTR_SKIP);
+
+        ob_start();
+
+        include $this->resolve($template);
+
+        $output = ob_get_contents();
+
+        ob_end_clean();
+
+        return $output;
+    }
+
+    public function resolve(string $path)
+    {
+        return "{$this->basePath}/{$path}";
     }
 }
