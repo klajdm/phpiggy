69b8833e96f9304fddfd5364c5b2964ca5bd01c9
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-21 15:50:24 +0200

    validation breakpoint

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Framework;
 
 use Framework\Contracts\RuleInterface;
+use Framework\Exceptions\ValidationException;
 
 class Validator
 {
@@ -15,7 +16,28 @@ class Validator
         $this->rules[$alias] = $rule;
     }
 
-    public function validate(array $formData)
+    public function validate(array $formData, array $fields)
     {
+        $errors = [];
+
+        foreach ($fields as $fieldName => $rules) {
+            foreach ($rules as $rule) {
+                $ruleValidator = $this->rules[$rule];
+
+                if ($ruleValidator->validate($formData, $fieldName, [])) {
+                    continue;
+                }
+
+                $errors[$fieldName][] = $ruleValidator->getMessage(
+                    $formData,
+                    $fieldName,
+                    []
+                );
+            }
+        }
+
+        if (count($errors)) {
+            throw new ValidationException();
+        }
     }
 }
