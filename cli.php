234e9dbc8967fc20653bf26fe072bcbe647f2410
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-22 13:47:35 +0200

    updated database

--- cli.php
+++ cli.php
@@ -1,8 +1,13 @@
 <?php
 
-include __DIR__ . "/src/Framework/Database.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use Framework\Database;
+use Dotenv\Dotenv;
+use App\Config\Paths;
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 $db = new Database($_ENV['DB_DRIVER'], [
     'host' => $_ENV['DB_HOST'],
