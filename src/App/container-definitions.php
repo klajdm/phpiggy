15150acaa1f4abae2b075f3f074814eb3fed8a02
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-22 17:28:50 +0200

    added logic for create, view, delete receipt

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\{ValidatorService, UserService, TransactionService};
+use App\Services\{ValidatorService, UserService, TransactionService, ReceiptService};
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
@@ -25,5 +25,10 @@ return [
         $db = $container->get(Database::class);
 
         return new TransactionService($db);
+    },
+    ReceiptService::class => function (Container $container) {
+        $db = $container->get(Database::class);
+
+        return new ReceiptService($db);
     }
 ];
