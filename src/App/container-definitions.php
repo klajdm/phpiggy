fac557dda7d830912c3e540858aadb47413c706e
Klajdi Murataj <klajdimurati3@gmail.com>
2024-07-22 13:54:14 +0200

    added create transaction logic

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\{ValidatorService, UserService};
+use App\Services\{ValidatorService, UserService, TransactionService};
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
@@ -14,9 +14,16 @@ return [
         'port' => $_ENV['DB_PORT'],
         'dbname' => $_ENV['DB_NAME']
     ], $_ENV['DB_USER'], $_ENV['DB_PASS']),
+
     UserService::class => function (Container $container) {
         $db = $container->get(Database::class);
 
         return new UserService($db);
+    },
+
+    TransactionService::class => function (Container $container) {
+        $db = $container->get(Database::class);
+
+        return new TransactionService($db);
     }
 ];
